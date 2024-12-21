64627063e35e61f6dc25239bf3b7fcd8073ab829
Sean McGrath <sean@mcgrath.net.nz>
2024-12-21 23:51:12 +1300

    feat: Implement comprehensive user CRUD operations with advanced features

--- api_service/app/crud/__init__.py
+++ api_service/app/crud/__init__.py
@@ -4,6 +4,7 @@ CRUD operations package.
 This module exports all CRUD operation instances.
 """
 
+from .crud_user import user
 from .crud_device import device
 
-__all__ = ["device"]
+__all__ = ["user", "device"]
