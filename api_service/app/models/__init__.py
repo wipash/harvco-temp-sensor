11a08389998a0f57031e417ee82d53a85c6b3c3e
Sean McGrath <sean@mcgrath.net.nz>
2024-12-22 22:12:37 +1300

    fix: Update model imports to use correct class names in __init__.py

--- api_service/app/models/__init__.py
+++ api_service/app/models/__init__.py
@@ -1,4 +1,7 @@
-from .user import user
-from .device import device
-from .reading import reading
-from .token import token
+from .user import User
+from .device import Device
+from .reading import Reading
+from .token import RefreshToken
+
+# Make all models available at the package level
+__all__ = ["User", "Device", "Reading", "RefreshToken"]
